edced8a94a0afb2b932de79b7288ef0cdd2a4e03
Jacob Bare <jacob@limit0.io>
2017-12-04 11:05:44 -0600

    Make `log` method private

--- src/tracker/index.js
+++ src/tracker/index.js
@@ -6,6 +6,28 @@ const { APP_NAME } = require('../constants');
 const commands = {};
 const { assign } = Object;
 
+/**
+ * Hooks into a command promise and logs the result or error.
+ *
+ * @todo Polyfill promises.
+ * @param {Logger} logger
+ * @param {Promise} promise
+ * @param {string} command
+ * @param {object} opts
+ * @return {Promise}
+ */
+const log = (logger, promise, command, opts) => {
+  const msg = stage => `Command execution ${stage} for '${command}'`;
+  logger.dispatch('log', msg('started'), opts);
+  return promise.then((result) => {
+    logger.dispatch('info', msg('complete'), result);
+    return result;
+  }).catch((error) => {
+    logger.dispatch('error', msg('error'), error);
+    return error;
+  });
+};
+
 module.exports = compose({
   init({ id, name, logger } = {}) {
     this.options = TrackerOptions({ id, name, logger });
@@ -29,28 +51,7 @@ module.exports = compose({
       const promise = typeof commands[command] === 'function' ?
         Promise.resolve(commands[command](this, opts)) :
         Promise.reject(new Error(`The command '${command}' was not found.`));
-      return this.log(promise, command, opts);
-    },
-
-    /**
-     * Hooks into a command promise and logs the result or error.
-     *
-     * @todo Polyfill promises.
-     * @param {Promise} promise
-     * @param {string} command
-     * @param {object} opts
-     * @return {Promise}
-     */
-    log(promise, command, opts) {
-      const msg = stage => `Command execution ${stage} for '${command}'`;
-      this.logger.dispatch('log', msg('started'), opts);
-      return promise.then((result) => {
-        this.logger.dispatch('info', msg('complete'), result);
-        return result;
-      }).catch((error) => {
-        this.logger.dispatch('error', msg('error'), error);
-        return error;
-      });
+      return log(this.logger, promise, command, opts);
     },
   },
 });
