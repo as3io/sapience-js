0e3b606e7eaf1668b4fc3c4ad31f94c917fa2867
Jacob Bare <jacob@limit0.io>
2017-12-07 12:43:49 -0600

    Remove callback support from the app
    Will need to revisit at a later date, as not passing a callback will prevent rejected promises (errors) from displaying

--- src/app.js
+++ src/app.js
@@ -10,36 +10,32 @@ const trackers = {};
  * @todo Polyfill promises.
  * @param {string} command The command name.
  * @param {object} options The command options.
- * @param {function} cb A callback function to executed on command success or failure.
  * @return {Promise}
  */
-module.exports = (command, options, cb) => {
-  const next = typeof cb === 'function' ? cb : () => undefined;
-  return new Promise((resolve, reject) => {
-    const opts = assign({}, options);
-    const cmd = String(command);
-    if (cmd === 'init') {
-      // Create the tracker instance on init.
-      const name = opts.name || DEFAULT_TRACKER_NAME;
-      opts.name = name;
-      if (!trackers[name]) {
-        // Only create the instance once.
-        try {
-          trackers[name] = Tracker(opts);
-          return resolve(trackers[name]);
-        } catch (e) {
-          return reject(e);
-        }
+module.exports = (command, options) => new Promise((resolve, reject) => {
+  const opts = assign({}, options);
+  const cmd = String(command);
+  if (cmd === 'init') {
+    // Create the tracker instance on init.
+    const name = opts.name || DEFAULT_TRACKER_NAME;
+    opts.name = name;
+    if (!trackers[name]) {
+      // Only create the instance once.
+      try {
+        trackers[name] = Tracker(opts);
+        return resolve(trackers[name]);
+      } catch (e) {
+        return reject(e);
       }
-      return resolve(trackers[name]);
     }
-    // Delegate the command to the appropriate tracker.
-    const parts = cmd.split('.', 2);
-    const params = {
-      name: (parts.length === 2) ? parts[0] : DEFAULT_TRACKER_NAME,
-      command: (parts.length === 2) ? parts[1] : parts[0],
-    };
-    const tracker = trackers[params.name];
-    return tracker ? resolve(tracker.execute(params.command, opts)) : reject(new Error(`No ${APP_NAME} tracker named '${params.name}' was found.`));
-  }).then(res => next(null, res)).catch(err => next(err));
-};
+    return resolve(trackers[name]);
+  }
+  // Delegate the command to the appropriate tracker.
+  const parts = cmd.split('.', 2);
+  const params = {
+    name: (parts.length === 2) ? parts[0] : DEFAULT_TRACKER_NAME,
+    command: (parts.length === 2) ? parts[1] : parts[0],
+  };
+  const tracker = trackers[params.name];
+  return tracker ? resolve(tracker.execute(params.command, opts)) : reject(new Error(`No ${APP_NAME} tracker named '${params.name}' was found.`));
+});
