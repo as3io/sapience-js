0676763083043c787643ff0894e8f6f30bd6085f
Jacob Bare <jacob@limit0.io>
2017-12-07 12:42:01 -0600

    Update sapience-core and add js-cookie

--- src/composables/loggable.js
+++ src/composables/loggable.js
@@ -1,11 +1,10 @@
 const compose = require('stampit');
 const Logger = require('../logger');
-const { stampTypeOf } = require('sapience-core').utils;
 
 const defaultLogger = Logger({ disbled: true });
 
 module.exports = compose({
   init({ logger } = {}) {
-    this.logger = stampTypeOf(logger) === 'logger' ? logger : defaultLogger;
+    this.logger = logger || defaultLogger;
   },
 });
